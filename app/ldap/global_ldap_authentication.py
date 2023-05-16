09d46a04512333992c4438088442c90d4d2693c8
lliwi <31549684+lliwi@users.noreply.github.com>
2023-05-16 20:09:53 +0200

    Add files via upload

--- app/ldap/global_ldap_authentication.py
+++ app/ldap/global_ldap_authentication.py
@@ -3,49 +3,74 @@ from ldap3.core.exceptions import LDAPException, LDAPBindError
 from flask import current_app
 
 def global_ldap_authentication(user_name, user_pwd):
-  
-# ldap server hostname and port
-    ldap_server = "ldap://"+ current_app.config['LDAP_SERVER'] + ":389"
+    # LDAP server hostname and port
+    ldap_server = "ldap://" + current_app.config['LDAP_SERVER'] + ":389"
 
-    # root dn
+    # Root DN
     root_dn = current_app.config['LDAP_ROOT_DN']
-	
-    # service account dn and password
+
+
+    # Service account DN and password
     service_dn = current_app.config['LDAP_USER']
     service_password = current_app.config['LDAP_PASSWORD']
 
 
-    # create server and connection objects
+    # Group DN to check membership
+    if current_app.config['LDAP_GROUP_DN']:
+        group_dn = current_app.config['LDAP_GROUP_DN']
+
+    # Create server and connection objects
+
     server = Server(ldap_server)
     conn = Connection(server, user=service_dn, password=service_password, authentication=SIMPLE)
 
     try:
-        # authenticate service account
+
+        # Authenticate service account
         conn.bind()
 
-        # search for the user
+ 
+
+        # Search for the user
         search_filter = f"({current_app.config['LDAP_ATTRIBUTE']}={user_name})"
         conn.search(search_base=root_dn, search_filter=search_filter, search_scope=SUBTREE)
-        
-        # check if the user was found
+
+       
+
+        # Check if the user was found
         if len(conn.entries) == 0:
             return "User not found"
 
-        # construct user_dn using the distinguishedName attribute of the user object
+ 
+
+        # Construct user_dn using the distinguishedName attribute of the user object
         user_dn = conn.entries[0].entry_dn
 
-        # authenticate user
+        # Authenticate user
         user_conn = Connection(server, user=user_dn, password=user_pwd, authentication=SIMPLE)
         user_conn.bind()
 
-        # check if the user authentication is successful
+        # Check if the user authentication is successful
         if not user_conn.bound:
             return "Invalid credentials"
 
-        # authentication successful
+ 
+
+        # Check group membership
+        if current_app.config['LDAP_GROUP_DN']:
+            group_filter = f"(&(objectClass=user)(distinguishedName={user_dn})(memberOf={group_dn}))"
+            conn.search(search_base=root_dn, search_filter=group_filter, search_scope=SUBTREE)
+ 
+
+        # If the user is not a member of the group, return an error message
+        if len(conn.entries) == 0:
+            return "User is not a member of the required group"
+
+        # Authentication and group membership validation successful
         return "Success"
 
+
+
     except LDAPException as e:
-        # authentication failed
+        # Authentication failed
         return f"LDAP error: {e}"
-    
